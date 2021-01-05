bf781a24be5c86aa6d083d62ecea94a8e48bc3bf
Raghendra kumar <50679225+raghvendra98@users.noreply.github.com>
2021-01-05 19:07:17 +0530

    Feat:integrated web page

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
-from flask import Flask , render_template
+from flask import Flask , render_template,request
+import pickle
+import numpy as np
+
+
 app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+col=(['MSSubClass', 'MSZoning', 'LotFrontage', 'LotArea', 'Street',
+       'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope',
+       'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle',
+       'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd', 'RoofStyle',
+       'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'MasVnrArea',
+       'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond',
+       'BsmtExposure', 'BsmtFinType1', 'BsmtFinSF1', 'BsmtFinType2',
+       'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'Heating', 'HeatingQC',
+       'CentralAir', 'Electrical', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF',
+       'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath',
+       'BedroomAbvGr', 'KitchenAbvGr', 'KitchenQual', 'TotRmsAbvGrd',
+       'Functional', 'Fireplaces', 'GarageType', 'GarageYrBlt', 'GarageFinish',
+       'GarageCars', 'GarageArea', 'GarageQual', 'GarageCond', 'PavedDrive',
+       'WoodDeckSF', 'OpenPorchSF', 'EnclosedPorch', '3SsnPorch',
+       'ScreenPorch', 'PoolArea', 'MiscVal', 'MoSold', 'YrSold', 'SaleType',
+       'SaleCondition'])
 
 @app.route('/')
 def hello():
     return render_template("index.html")
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final = [np.array(int_features, dtype=int)]
+    prediction = model.predict(final)[0][0].round(2)
+
+    return render_template('index.html', predict_price='The price of your dream house is {}.'.format(prediction))
+
 
 app.run(debug=True)
